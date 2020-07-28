12ab52830c1d0d76f2578bfb47fb92013af1eddc
aditya gupta <adityasgupta309@gmail.com>
2020-07-28 20:48:17 +0530

    node file sys

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+<div @submit.prevent="getData">
+<employee />
+ <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="process" name="submit" ><br />
+  <textarea disabled style="overflow:auto;resize:none" rows="23" cols="50" v-model="message" />
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data(){
+    return{
+      data:{}
+    }
+  },
+  methods: {
+    async getName(){
+      const { data } = await axios.get("https://{{}}/?name=michael");
+      this.data = data;
+    }
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import fs from 'fs'
+
+Vue.use(fs)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
